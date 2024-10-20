5adceb037bcfc2158112206764183f81926dcf8b
ValBaron10 <valentin.baron10@gmail.com>
2024-10-20 12:00:18 -0500

    Update path to put Pathlib

--- jeu.py
+++ jeu.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import os
+from pathlib import Path
 from PIL import Image
 
 # Dossier contenant les sons et les images
@@ -28,14 +29,14 @@ def mini_jeu():
 
     # Affiche le son actuel
     son = sons[st.session_state.son_index]
-    st.audio(os.path.join(SONS_DIR, son))
+    st.audio(os.path.join(Path(__file__).parents[0], os.path.join(SONS_DIR, son)))
 
     # Affiche les images
     st.write("Choisissez l'image correspondante :")
     image_cols = st.columns(len(images))
 
     for col, image in zip(image_cols, images):
-        img = Image.open(os.path.join(IMAGES_DIR, image))
+        img = Image.open(os.path.join(Path(__file__).parents[0], os.path.join(IMAGES_DIR, image)))
         col.image(img, caption=image, use_column_width=True)
 
     # Sélection de l'image associée
